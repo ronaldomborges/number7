3de2f129c7ef4e561018d007da3acfa87eff6908
ronaldomborges <65875791+ronaldomborges@users.noreply.github.com>
2021-04-19 17:51:28 -0300

    1

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+function relogio() {
+    function CriaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000)
+        return data.toLocaleTimeString('pt-br', {
+            hour12: false,
+            timeZone: 'UTC'
+        })
+    }
+    
+    
+    const relogio = document.querySelector('.relogio')
+    const iniciar = document.querySelector('.iniciar')
+    const pausar = document.querySelector('.pausar')
+    const zerar = document.querySelector('.zerar')
+    let segundos = 0
+    let timer
+    
+    function iniciaRelogio () {
+       timer = setInterval(() => {
+           segundos++
+           relogio.innerHTML =  CriaHoraDosSegundos(segundos)
+        }, 1000);
+    }
+    
+    document.addEventListener('click', function(e) {
+        const el = e.target
+        
+        if(el.classList.contains('zerar')) {
+            clearInterval(timer)
+            relogio.innerHTML = '00:00:00'
+            relogio.classList.remove('pausado')
+            segundos = 0
+        }
+    
+        if(el.classList.contains('iniciar')) {
+           relogio.classList.remove('pausado')
+           clearInterval(timer)
+           iniciaRelogio() 
+        }
+    
+        if(el.classList.contains('pausar')) {
+           clearInterval(timer)
+           relogio.classList.add('pausado') 
+        }
+    })  
+}
+relogio()
+
+
